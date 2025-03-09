c6d3a70217d94c97503a4e375c0f09ecac55d826
Tyler Riker <tyler@wewritecode.com>
2025-03-09 03:19:13 -0500

    add server files; add modpack; patch forge

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -24,7 +24,7 @@ echo "installing deps"
 # Update package lists
 apt update
 # Install curl
-apt install -y curl
+apt install -y curl rsync
 apt install -y bc binutils bsdmainutils distro-info jq lib32gcc-s1 lib32stdc++6 netcat-openbsd openjdk-17-jre pigz tmux unzip uuid-runtime
 
 #switch to mcserver
@@ -34,6 +34,40 @@ cd /home/mcserver
 cp ${current_dir}/install-linuxgsm.sh .
 sudo -u mcserver ./install-linuxgsm.sh
 rm install-linuxgsm.sh
+echo "copy server files"
+rsync -a --remove-source-files ${current_dir}/server-files/ /home/mcserver
+
+modpack_name="v4.5_SERVER_TAD"
+echo "download ANCIENT DAWN!!!"
+if [ -f "${modpack_name}.zip" ]; then
+    echo "Download already exists."
+else
+    curl -Lo ${modpack_name}.zip 'https://mediafilez.forgecdn.net/files/6282/758/v4.5_SERVER_TAD.zip' \
+    -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+    -H 'accept-language: en-US,en;q=0.9' \
+    -H 'cache-control: no-cache' \
+    -H 'dnt: 1' \
+    -H 'pragma: no-cache' \
+    -H 'priority: u=0, i' \
+    -H 'referer: https://www.curseforge.com/' \
+    -H 'sec-ch-ua: "Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"' \
+    -H 'sec-ch-ua-mobile: ?0' \
+    -H 'sec-ch-ua-platform: "macOS"' \
+    -H 'sec-fetch-dest: document' \
+    -H 'sec-fetch-mode: navigate' \
+    -H 'sec-fetch-site: cross-site' \
+    -H 'sec-fetch-user: ?1' \
+    -H 'upgrade-insecure-requests: 1' \
+    -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
+fi
+unzip ${modpack_name}
+rsync -a --remove-source-files "Server TAD"/* serverfiles
+rm -r "Server TAD"
+
+echo "Patching in Forge"
+cd serverfiles
+java -jar forge-1.20.1-47.4.0-installer.jar --install-server
+
 chown -R mcserver:mcserver /home/mcserver
 cd "${current_dir}"
 
--- install-to.sh
+++ install-to.sh
@@ -10,12 +10,14 @@ remote_host=$1
 
 echo "copy installs to ${remote_host}"
 scp install-scripts/* "${remote_host}":
+scp -r server-files/* "${remote_host}":server-files
 
 echo "execute installs on ${remote_host}"
 ssh -t $remote_host "sudo stdbuf -oL -eL bash ./install.sh"
 
 echo "delete installs on ${remote_host}"
 ssh $remote_host "rm install.sh install-linuxgsm.sh"
+ssh $remote_host "rm -rf server-files"
 
 echo "exit with great success"
 exit 0
