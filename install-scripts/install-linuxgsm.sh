9e5f989a79aed4de02577166577d1ebb52664ef0
Tyler Riker <tyler@wewritecode.com>
2025-03-09 11:34:01 -0500

    don't isntall mcserver if already exists

--- install-scripts/install-linuxgsm.sh
+++ install-scripts/install-linuxgsm.sh
@@ -2,8 +2,12 @@
 
 #download linuxgsm
 echo "download linuxgsm"
-curl -Lo linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
+if [ -f "mcserver" ]; then
+    echo "mcserver file already exists."
+else
+    curl -Lo linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
 
-#install linuxgsm
-echo "install linuxgsm"
-./mcserver install
+    #install linuxgsm
+    echo "install linuxgsm"
+    ./mcserver install
+fi
