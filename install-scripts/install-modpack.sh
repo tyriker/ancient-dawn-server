8db97e1b1efa505170a45b3946763dab8b3ff7b0
Tyler Riker <tyriker@users.noreply.github.com>
2025-07-27 14:14:40 -0500

    Update to make safer

--- install-scripts/install-modpack.sh
+++ install-scripts/install-modpack.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 modpack_name="v4.5_SERVER_TAD"
-echo "download ANCIENT DAWN"
+root_dir_in_modpack="Server TAD"
+echo "download modpack"
 if [ -f "${modpack_name}.zip" ]; then
     echo "Download already exists."
 else
-    curl -Lo ${modpack_name}.zip 'https://mediafilez.forgecdn.net/files/6282/758/v4.5_SERVER_TAD.zip' \
+    curl -Lo "${modpack_name}.zip" 'https://mediafilez.forgecdn.net/files/6282/758/v4.5_SERVER_TAD.zip' \
     -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
     -H 'accept-language: en-US,en;q=0.9' \
     -H 'cache-control: no-cache' \
@@ -23,9 +24,9 @@ else
     -H 'upgrade-insecure-requests: 1' \
     -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
 fi
-unzip ${modpack_name}
-rsync -a --remove-source-files "Server TAD"/* serverfiles
-rm -r "Server TAD"
+unzip "${modpack_name}.zip"
+rsync -a --remove-source-files "${root_dir_in_modpack}"/* serverfiles
+rm -r "${root_dir_in_modpack}"
 
 #remove client-only mods
 rm serverfiles/mods/sodiumextras-forge-1.0.7-1.20.1.jar
