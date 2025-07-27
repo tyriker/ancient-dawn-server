9e4f3ef62b38051e837723d5322edd2ad8d96158
Tyler Riker <tyriker@users.noreply.github.com>
2025-07-27 14:17:54 -0500

    Update to be idempotent

--- install-scripts/install-service.sh
+++ install-scripts/install-service.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-if systemctl list-unit-files | grep -q '^mcserver\.service'; then
-    echo "mcserver service already exists."
-else
-    SERVICE_FILE="/etc/systemd/system/mcserver.service"
+# doesn't matter if it already exists, just always overwrite and reload
+SERVICE_FILE="/etc/systemd/system/mcserver.service"
 
-    echo "[Unit]
-    Description=Minecraft Server
-    After=network-online.target
-    Wants=network-online.target
+echo "[Unit]
+Description=Minecraft Server
+After=network-online.target
+Wants=network-online.target
 
-    [Service]
-    Type=forking
-    User=mcserver
-    #Assume that the service is running after main process exits with code 0
-    RemainAfterExit=yes
-    ExecStart=/home/mcserver/mcserver start
-    ExecStop=/home/mcserver/mcserver stop
-    Restart=no
-    WorkingDirectory=/home/mcserver
+[Service]
+Type=forking
+User=mcserver
+#Assume that the service is running after main process exits with code 0
+RemainAfterExit=yes
+ExecStart=/home/mcserver/mcserver start
+ExecStop=/home/mcserver/mcserver stop
+Restart=no
+WorkingDirectory=/home/mcserver
 
-    [Install]
-    WantedBy=multi-user.target" > $SERVICE_FILE
+[Install]
+WantedBy=multi-user.target" > $SERVICE_FILE
 
-    chmod 644 $SERVICE_FILE
-    systemctl daemon-reload
-fi
+chmod 644 $SERVICE_FILE
 
+systemctl daemon-reload
 systemctl enable mcserver
 systemctl start mcserver
